a4e0e9eeaeb60162e98bd156b80367509f60de2a
Alan Reid <alanreidt@gmail.com>
2022-10-30 09:58:38 +0400

    Realize cells rendering

--- src/Board.tsx
+++ src/Board.tsx
@@ -1,43 +1,27 @@
-import React from "react";
+import React, { useState } from "react";
 import styles from "./Board.module.css";
 
-export function Board() {
+export function Board({ size = 3 }) {
+  const [state, setState] = useState<(boolean | undefined)[][]>(() =>
+    Array(size)
+      .fill(undefined)
+      .map((value) => Array(size).fill(value))
+  );
+
   return (
     <table className={styles.Board}>
       <tbody>
-        <tr>
-          <td className={styles.Cell}>
-            <button className={styles.Button}>X</button>
-          </td>
-          <td className={styles.Cell}>
-            <button className={styles.Button}>O</button>
-          </td>
-          <td className={styles.Cell}>
-            <button className={styles.Button}>X</button>
-          </td>
-        </tr>
-        <tr>
-          <td className={styles.Cell}>
-            <button className={styles.Button}>O</button>
-          </td>
-          <td className={styles.Cell}>
-            <button className={styles.Button}>X</button>
-          </td>
-          <td className={styles.Cell}>
-            <button className={styles.Button}>X</button>
-          </td>
-        </tr>
-        <tr>
-          <td className={styles.Cell}>
-            <button className={styles.Button}>O</button>
-          </td>
-          <td className={styles.Cell}>
-            <button className={styles.Button}>O</button>
-          </td>
-          <td className={styles.Cell}>
-            <button className={styles.Button}>X</button>
-          </td>
-        </tr>
+        {state.map((row) => (
+          <tr>
+            {row.map((item) => (
+              <td className={styles.Cell}>
+                <button className={styles.Button}>
+                  {item !== undefined && (item ? "X" : "O")}
+                </button>
+              </td>
+            ))}
+          </tr>
+        ))}
       </tbody>
     </table>
   );
